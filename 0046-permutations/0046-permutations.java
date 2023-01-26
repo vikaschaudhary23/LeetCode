ac1d5fe811d8a7e9e888948eda7fff1cc44fe29e
Vikas Chaudhary <vikaschaudhary2309@gmail.com>
2023-01-26 11:19:43 +0530

    Time: 1 ms (92.44%), Space: 42.5 MB (69.66%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,27 +1,25 @@
 class Solution {
-    public void permutations(boolean[] arr, int[] a, List<List<Integer>> ans, List<Integer> l, int size){
-        if(l.size()==size){
+    public void swap(int i, int j, int[] a){
+        int t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+    public void permutations2(int index, int[] a, List<List<Integer>> ans, int size){
+        if(index==size){
+            List<Integer> l = new ArrayList<>();
+            for(int j:a) l.add(j);
             ans.add(new ArrayList<>(l));
             return;
         }
-        for(int i=0;i<size;i++){
-            if(!arr[i]){
-                arr[i] = true;
-                l.add(a[i]);
-                permutations(arr, a, ans, l, size);
-                l.remove(l.size()-1);
-                arr[i] = false;
-            }
+        for(int i=index;i<size;i++){
+            swap(i,index,a);
+            permutations2(index+1, a, ans, size);
+            swap(i, index, a);
         }
-
     }
     public List<List<Integer>> permute(int[] nums) {
-        int size = nums.length;
-        boolean arr[] = new boolean[size];
-        Arrays.fill(arr,false);
-        List<Integer> l = new ArrayList<>();
         List<List<Integer>> ans = new ArrayList<>();
-        permutations(arr, nums, ans, l, size);
+        permutations2(0, nums, ans, nums.length);
         return ans;
     }
 }
