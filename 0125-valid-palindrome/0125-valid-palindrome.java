5d48f2fac3307bff7e4e94077b74a0e871ee59e0
Vikas Chaudhary <vikaschaudhary2309@gmail.com>
2023-02-21 08:53:35 +0530

    Time: 3 ms (96.77%), Space: 42.3 MB (76.75%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,31 @@
+import java.lang.*;
+class Solution {
+    
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+        // char a[] = s.toCharArray();
+        // ArrayList<Character> list = new ArrayList<>();
+        // for(int i=0;i< a.length;i++){
+        //     if((a[i]>96 && a[i]<123) || (a[i]<58 && a[i]>47)) list.add(a[i]);
+        // }
+        // int start=0,end=list.size()-1;
+        // while(start<=end) {
+        //     if (list.get(start) != list.get(end)) return false;
+        //     start++;
+        //     end--;
+        // }
+        // return true;
+        
+        int l=0,r=s.length()-1;
+        while(l<=r){
+            if(!Character.isLetterOrDigit(s.charAt(l))) l++;
+            else if(!Character.isLetterOrDigit(s.charAt(r))) r--;
+            else{
+            if(s.charAt(l)!=s.charAt(r)) return false;
+                l++;
+                r--;
+            }
+        }
+        return true;
+    }
+}
