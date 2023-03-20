2bd086415716239867c10a27142dc82ab48375ae
Vikas Chaudhary <vikaschaudhary2309@gmail.com>
2023-03-20 22:11:00 +0530

    Time: 0 ms (100.00%), Space: 39.1 MB (67.50%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -2,15 +2,11 @@ class Solution {
     public int ans(int n, int a[]){
         if(n==0 || n==1) return n;
         if(a[n]!=0) return a[n];
-        
-        a[n] = ans(n-1, a)+ans(n-2, a);
+        a[n] = ans(n-1,a)+ans(n-2,a);
         return a[n];
     }
     public int fib(int n) {
         int a[] = new int[n+1];
-        return ans(n, a);
-        // if(n<=1)
-        //     return n;
-        // return fib(n-1)+fib(n-2);
+        return ans(n,a);
     }
 }
