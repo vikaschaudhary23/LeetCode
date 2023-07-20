f844341e9b4cebb79251fff933e309211d97ac18
Vikas Chaudhary <vikaschaudhary2309@gmail.com>
2023-07-20 11:29:37 +0530

    Added solution - LeetHub

---
+++ Non Repeating Character - GFG/non-repeating-character.java
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Driverclass
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            String st = sc.next();
+            
+            char ans=new Solution().nonrepeatingCharacter(st);
+            
+            if(ans!='$')
+            System.out.println(ans);
+            else
+            System.out.println(-1);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    //Function to find the first non-repeating character in a string.
+    static char nonrepeatingCharacter(String S)
+    {
+        Map<Character, Integer> map = new HashMap<>();
+        for(int i=0;i<S.length();i++){
+            char c = S.charAt(i);
+            if(!map.containsKey(c)) map.put(c,1);
+            else{
+                int f = map.get(c);
+                f++;
+                map.put(c,f);
+            }
+        }
+        // System.out.println(map);
+        // System.out.println(map.get('h'));
+        for(int i=0;i<S.length();i++){
+            char c = S.charAt(i);
+            if(map.get(c)==1) return c;
+        }
+        return '$';
+        //Your code here
+    }
+}
+
