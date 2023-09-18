49273eb889e0dabb041d6b18c5bf964d77280021
Vikas Chaudhary <vikaschaudhary2309@gmail.com>
2023-09-18 20:39:54 +0530

    Added solution - LeetHub

--- Longest Common Subsequence - GFG/longest-common-subsequence.java
+++ Longest Common Subsequence - GFG/longest-common-subsequence.java
@@ -30,12 +30,15 @@ class Solution
     static int lcs(int x, int y, String s1, String s2)
     {
         int dp[][] = new int[x+1][y+1];
-        for(int i=0;i<=x;i++) dp[i][0] = 0;
-        for(int i=0;i<=y;i++) dp[0][i] = 0;
-        
+        for(int i=0;i<y;i++){
+            dp[0][i] = 0;
+        }
+        for(int i=0;i<x;i++){
+            dp[i][0] = 0;
+        }
         for(int i=1;i<=x;i++){
             for(int j=1;j<=y;j++){
-                if(s1.charAt(i-1) == s2.charAt(j-1)) dp[i][j] = 1 + dp[i-1][j-1];
+                if(s1.charAt(i-1)==s2.charAt(j-1)) dp[i][j] = 1 + dp[i-1][j-1];
                 else dp[i][j] = Math.max(dp[i][j-1], dp[i-1][j]);
             }
         }
